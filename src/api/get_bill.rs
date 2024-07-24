f0308122c88991fc01dfda853dc10afcf3bd27fa
Wiley Conte <wileymckayconte@gmail.com>
2024-07-23 22:20:10 -0600

    minor update

--- src/api/get_bill.rs
+++ src/api/get_bill.rs
@@ -10,6 +10,7 @@ pub struct GetBillResponse {
 #[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[cfg_attr(feature = "async-graphql", derive(async_graphql::SimpleObject))]
 #[graphql(complex)]
+/// Legiscan Bill object
 pub struct Bill {
     pub bill_id: i32,
     pub change_hash: String,
